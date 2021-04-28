07e312e10014e93b29d7dfac41fb85b144a1dbd7
ThrasyvoulosK <69692688+ThrasyvoulosK@users.noreply.github.com>
2021-04-28 17:32:18 +0300

    Daily 28/4/2021
    Added tutorial graphics and made a series of prefabs for it.

---
+++ Assets/TutorialScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    public List<Sprite> tutorialSprites;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
